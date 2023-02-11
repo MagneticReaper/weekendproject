dd3e8107dccb17c14e72fb988de6aa5bb2492bc2
Remy Bourdon <remybourdon@outlook.com>
2023-02-11 16:47:26 -0500

    progress

---
+++ src/main/java/ca/vanier/weekendproject/entity/Item.java
@@ -0,0 +1,53 @@
+package ca.vanier.weekendproject.entity;
+
+import java.util.List;
+
+import ca.vanier.Product;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String description;
+    private int quantity;
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Product> product;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public List<Product> getProduct() {
+        return product;
+    }
+
+    public void setProduct(List<Product> product) {
+        this.product = product;
+    }
+}
