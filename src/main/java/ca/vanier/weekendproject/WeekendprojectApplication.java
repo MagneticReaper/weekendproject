0205be59693e691c6c4a889f7604f16f9ba0d7b5
Remy Bourdon <remybourdon@outlook.com>
2023-02-11 15:02:24 -0500

    first commit

---
+++ src/main/java/ca/vanier/weekendproject/WeekendprojectApplication.java
@@ -0,0 +1,13 @@
+package ca.vanier.weekendproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekendprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeekendprojectApplication.class, args);
+    }
+
+}
